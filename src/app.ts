8566c61d4c973c39c43a919bf9b19ca20a42e3e5
Sumanpreet SIngh <ssingh28@rrc.ca>
2026-03-01 05:42:59 -0500

    Updated app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import express, { Express } from "express";
 import healthRoutes from "./api/v1/routes/healthRoutes";
+import eventRoutes from "./api/v1/routes/eventRoutes";
 
 // Initialize Express application
 const app: Express = express();
 
+// Middleware
+app.use(express.json());
+
 // Define a route
 app.use("/api/v1", healthRoutes);
+app.use("/api/v1/events", eventRoutes);
 
 export default app;
