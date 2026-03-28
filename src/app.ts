c624c8d4cf8ede65fb00762e8f5a1ba5e7d3c779
Sumanpreet SIngh <ssingh28@rrc.ca>
2026-03-28 01:11:15 -0400

    Update app.ts to use enviorement variables

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,9 @@
 import express, { Express } from "express";
+import dotenv from "dotenv";
+
+// Load environment variables BEFORE your internal imports!
+dotenv.config();
+
 import healthRoutes from "./api/v1/routes/healthRoutes";
 import eventRoutes from "./api/v1/routes/eventRoutes";
 
