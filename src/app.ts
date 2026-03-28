825177f8308a955a67d64cc523a5491e843fb50e
Sumanpreet SIngh <ssingh28@rrc.ca>
2026-03-28 03:36:57 -0400

    Updated helmet middleware into express app

--- src/app.ts
+++ src/app.ts
@@ -4,16 +4,20 @@ import dotenv from "dotenv";
 // Load environment variables BEFORE your internal imports!
 dotenv.config();
 
+import { getHelmetConfig } from "../config/helmetConfig";
 import healthRoutes from "./api/v1/routes/healthRoutes";
 import eventRoutes from "./api/v1/routes/eventRoutes";
 
 // Initialize Express application
 const app: Express = express();
 
-// Middleware
+// Security middleware
+app.use(getHelmetConfig());
+
+// Body parsing middleware
 app.use(express.json());
 
-// Define a route
+// Routes
 app.use("/api/v1", healthRoutes);
 app.use("/api/v1/events", eventRoutes);
 
